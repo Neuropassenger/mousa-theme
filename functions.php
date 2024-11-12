9b0f0c1c593b288b356715d8ccdc178cea7e2b4e
neuropassenger <turgenoid@gmail.com>
2024-11-12 18:47:31 +0400

    Change marker style for first-level list items

--- functions.php
+++ functions.php
@@ -12,4 +12,10 @@ function mousa_enqueue_block_styles() {
 		'src'    => get_theme_file_uri( "assets/css/blocks/core-quote.css" ),
 		'path'   => get_theme_file_path( "assets/css/blocks/core-quote.css" )
 	) );
+
+    wp_enqueue_block_style( 'core/list', array(
+		'handle' => 'mousa-block-list',
+		'src'    => get_theme_file_uri( "assets/css/blocks/core-list.css" ),
+		'path'   => get_theme_file_path( "assets/css/blocks/core-list.css" )
+	) );
 }
